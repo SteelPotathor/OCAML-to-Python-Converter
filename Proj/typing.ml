6a4031f021dab0e918e9f00c9e35994c1d66896f
Thorgrimmneth <98515992+Thorgrimmneth@users.noreply.github.com>
2024-04-03 23:00:50 +0200

    Update typing.ml

--- Proj/typing.ml
+++ Proj/typing.ml
@@ -11,7 +11,7 @@ exception NotFound;;
 let tp_const = function
     | BoolV _ -> BoolT
     | IntV _ -> IntT
-    | UndefV -> UndefV;;
+    | UndefV -> failwith "Impossible de déterminer le type d'une valeur non définie";;
 
 (* search the type of v in env.localvar *)
 let search_tp_localvar v env = 
@@ -23,26 +23,27 @@ in aux env;;
 (* search the type of v in env.funbind *)
 let search_tp_funbind v env = 
     let rec aux = function
-        | (a, b)::c -> if v = a then fst b else aux c
+        | (a, FPdecl(tp,name,l))::c -> if v = a then FPdecl(tp,name,l) else aux c
         | _ -> raise NotFound
 in aux env;; 
 
 (* create a function type *)
 let type_creation = function 
-    | FPdecl(t_retour, _, (Vardecl(a, b)::c) as vars) -> 
+    | FPdecl(t_retour, _, (Vardecl(a, b)::c)) -> 
         let rec aux = function 
-            | Vardecl(_, b) -> b
-    		| Vardecl(_, b)::c -> FunT(b, aux c) 
-    in FunT(aux vars, t_retour)
+            | [] -> b
+            | [Vardecl(_, x)] -> FunT(b,x)
+    		| Vardecl(_, x)::c -> FunT(x, aux c) 
+    in FunT(aux c, t_retour)
     | FPdecl(t_retour, _, _) -> t_retour;;
 
 (* type a variable within env *)
-let tp_var env v = try type_creation (search_tp_var v env.localvar) with | NotFound -> try type_creation (search_tp_funbind v env.funbind) with | NotFound -> failwith "echec de typage, la variable n'est pas dans l'environnement";;
+let tp_var env v = try (search_tp_localvar v env.localvar) with | NotFound -> try type_creation (search_tp_funbind v env.funbind) with | NotFound -> failwith "echec de typage, la variable n'est pas dans l'environnement";;
 
 (* type of a function with its parameters *)
 let rec function_type_correct tf = function
-    | (a::b) -> match tf with | FunT(t1, t2) -> if t1 = a then function_type_correct t2 b else failwith "erreur le type du paramètre et le type de la fonction ne correspondent pas"
-                              | _ -> failwith "erreur application d'une non fonction"
+    | (a::b) -> (match tf with | FunT(t1, t2) -> if t1 = a then function_type_correct t2 b else failwith "erreur le type du paramètre et le type de la fonction ne correspondent pas"
+                              | _ -> failwith "erreur application d'une non fonction")
     | [] -> tf;; 
 
 (* type an expression within env *)
@@ -59,13 +60,15 @@ let rec tp_expr env = function
 
 let fun_bind fdfs = List.map (fun (Fundefn(fd, _)) -> (name_of_fpdecl fd, fd) ) fdfs;;
 
-let environment_initial fdfs = {localvar = []; funbind = fun_bind fdfs};;
+let environment_initial fdfs = {localvar = []; funbind = fun_bind fdfs} ;;
 
-let tp_prog (Prog (fdfs, e)) = let initial = environment_initial fdfs in List.for_all (fun fd -> tp_defn initial fd) fdfs ;;
+let convert_vardecl_list (vardecl_list : vardecl list) : (vname * tp) list =
+    List.map (fun (Vardecl (vname, tp)) -> (vname, tp)) vardecl_list ;;
 
-(* check if the function is correctly type and add it to the local environment *)
-let tp_fdefn env Fundefn(FPdecl(tf, _, params), e) = 
-    let envlocal = params @ env.localvar in
-    let te = tp_expr envlocal e in
-    te = tf;;
+(* check if the function is correctly type and add it to the local environment *)    
+let tp_fdefn env (Fundefn(FPdecl(tf, _, params), e)) = 
+    let envlocal = convert_vardecl_list params @ env.localvar in let te = tp_expr {localvar = envlocal; funbind = env.funbind} e in te = tf;;
+
+let tp_prog (Prog (fdfs, e)) = let initial = environment_initial fdfs in List.for_all (fun fd -> (tp_fdefn initial fd)) fdfs ;;
+
 
