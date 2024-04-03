be396d58ba90ad32631449fde8540f362eb26f56
SteelPotathor <97548610+SteelPotathor@users.noreply.github.com>
2024-04-03 21:36:37 +0200

    Add files via upload

--- Proj/eval.ml
+++ Proj/eval.ml
@@ -1,22 +1,21 @@
 open Lang
 
-(* ************************************************************ *)
-(* ****  Operational Semantics                             **** *)
-(* ************************************************************ *)
-
-(* ****  Auxiliary functions                               **** *)
-
-
-(* ****  Result of evaluation                              **** *)
-
 type result =
     Val of value
   | Closure of fpdefn * (result list)
 
+(* environment *)
 type state = (vname * result) list
 
 let eval_barith b v1 v2 boo = match v1, v2 with | Val(IntV i1), Val(IntV, i2) -> if boo && v2 = 0 then failwith "Division par zéro" else Val(IntV(op v1 v2)) 
-                                                | _ -> failwith "L'opération " ^ b ^ " s'effectue entre deux entiers"));;
+                                                | _ -> failwith "L'opération " ^ b ^ " s'effectue entre deux entiers";;
+
+let eval_blogic b v1 v2 = match v1, v2 with | Val(BoolV b1), Val(BoolV b2) -> Val(BoolV(b b1 b2))
+                                            | _ -> failwith "L'opération " ^ b ^ " s'effectue entre deux booléens";;
+
+let eval_bcompar b v1 v2 = match v1, v2 with | Val(IntV i1), Val(IntV, i2) -> Val(BoolV(b i1 i2))
+                                             | Val(BoolV b1), Val(BoolV b2) -> Val(BoolV(b b1 b2))
+                                             | _ -> failwith "L'opération " ^ b ^ " s'effectue entre deux entiers ou deux booléens";;
 
 let rec eval_expr env = function 
         | Const v -> Val v
@@ -25,18 +24,30 @@ let rec eval_expr env = function
                                                                                                     | BArith BAsub -> eval_barith (-) v1 v2 false
                                                                                                     | BArith BAmul -> eval_barith ( * ) v1 v2 false
                                                                                                     | BArith BAdiv -> eval_barith (/) v1 v2 true
-                                                                                                    | BArith BAmod -> eval_barith (%) v1 v2) false
+                                                                                                    | BArith BAmod -> eval_barith (mod) v1 v2 false
+                                                                                                    | BLogic BLand -> eval_blogic (&&) v1 v2
+                                                                                                    | BLogic BLor -> eval_blogic (||) v1 v2
+                                                                                                    | BCompar BCeq -> eval_bcompar (=) v1 v2
+                                                                                                    | BCompar BCge -> eval_bcompar (>=) v1 v2
+                                                                                                    | BCompar BCgt -> eval_bcompar (>) v1 v2
+                                                                                                    | BCompar BCle -> eval_bcompar (<=) v1 v2
+                                                                                                    | BCompar BClt -> eval_bcompar (<) v1 v2
+                                                                                                    | BCompar BCne -> eval_bcompar (!=) v1 v2)
         | IfThenElse(e1, e2, e3) -> (match eval_expr e1 with | Val(BoolV true) -> eval_expr env e2
                                                              | Val(BoolV false) -> eval_expr env e3
                                                              | _ -> failwith "La condition n'est pas un booléen")
-        | CallE(a::b) -> let eval_param = List.map (eval_expr env) b and Some(Closure(fp, _)) = eval_expr a in match fp with | Fundefn() (* je sais pas *) (* pas assez d'args, on crée une closure avec le nombre restant, trop = erreur, parfait = evalutation *)
-    ;;
+        | CallE(a::b) -> let eval_f = eval_expr env a and eval_param = List.map (eval_expr env) b in 
+            (match eval_f with | Closure(fp, _) -> (match fp with | Fundefn(fpdecl, e) -> let rec apply_params varlist eval_param envlocal used = 
+                                                                                            (match with varlist, eval_param | [], [] -> eval_expr envlocal e
+                                                                                            | (Vardecl(name, _)::b), x::y -> apply_params b y ((name, x)::envlocal)
+                                                                                            | (Vardecl(name, _)::b), _ -> Closure(Fundefn(fpdecl, e), List.rev used)
+                                                                                            | _ -> failwith "Trop de paramètres appliqués à la fonction")
+                                                                                          in apply_params (params_of_fpdecl fpdecl) eval_param env []
+                                                                  | Procdefn _ -> failwith "Une procédure ne peut être appelée")
+                               | _ -> failwith "Application à une non fonction") (* This should never happen if the program is correctly typed *);;
 
-(* Work in progress, almost done*)
 let eval_fpdefn env = function
         | Fundefn(FPdecl(tp, name, l), e) -> (name, eval_expr e) :: env
-        | Procdefn -> env (* to implement ? *);;
+        | Procdefn _ -> failwith "Une procédure ne peut être appelée" (* to implement ? *);;
 
-(* Work in progress => use list.folr to acc into the env and evaluate the expr with this env, seems okay i guess *)
 let eval_prog (Prog (fdfs, e)) = let env = List.fold_right (fun x f -> eval_fpdefn x f) [] fdfs in Val(eval_expr env e);;
-;;
